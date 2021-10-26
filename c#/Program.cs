1d05f1bf46e909e4d346d685f456b6e583aaae29
vino04003 <89254939+vino04003@users.noreply.github.com>
2021-10-26 14:02:06 +0200

    Add files via upload

---
+++ c#/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Nummerkul
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random press = new Random();
+            int returnvalue = press.Next(1, 100);
+
+            Console.WriteLine("Skriv ett heltal mellan 1 - 100: ");
+            int innit = int.Parse(Console.ReadLine());
+            int counter = 1;
+
+            while(innit != returnvalue)
+            {
+                counter=counter + 1;
+
+                if(innit < returnvalue)
+                {
+                    Console.WriteLine("Hmm, jag tror du får gissa högre.");
+                    innit = int.Parse(Console.ReadLine());
+                }
+
+                if(innit > returnvalue)
+                {
+                    Console.WriteLine("Oj! Inte så stort, gissa mindre tack.");
+                    innit = int.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("Vad bra!" + " Det tog bara "+ counter + " försök.");
+        }
+       
+    }
+}
