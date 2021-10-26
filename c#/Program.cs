827976ea64b8a169d7b85166626ae41f00aa9238
vino04003 <89254939+vino04003@users.noreply.github.com>
2021-10-26 13:50:09 +0200

    Add files via upload

---
+++ c#/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Nummerkul
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random slump = new Random();
+            int returnvalue = slump.Next(1, 100);
+
+            Console.WriteLine("Skriv ett yäni heltal mellan 1 o 100: ");
+            int clabb = int.Parse(Console.ReadLine());
+            int counter = 1;
+
+            while(clabb != returnvalue)
+            {
+                counter=counter + 1;
+
+                if(clabb < returnvalue)
+                {
+                    Console.WriteLine("Talet är mycket för lite");
+                    clabb = int.Parse(Console.ReadLine());
+                }
+
+                if(clabb > returnvalue)
+                {
+                    Console.WriteLine("Talet är mycket större än det minsta");
+                    clabb = int.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("Vad bra!" + " Det tog "+ counter + " försök.");
+        }
+       
+    }
+}
